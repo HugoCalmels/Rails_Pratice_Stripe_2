6251ccdcef691674a783fc3e5708678ad22cb638
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-12-03 21:28:16 +0100

    success and cancel paths for stripe checkout

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :products
   post "checkout/create", to: "checkout#create"
   resources :webhooks, only: [:create]
+
+  get "success", to: "checkout#success"
+  get "failure", to: "checkout#failure"
 end
