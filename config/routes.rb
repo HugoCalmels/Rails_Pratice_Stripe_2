c5b1172fe0cb091bc6f18ff234b973d09ff68d9f
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-12-03 04:16:30 +0100

    small fix on webhooks route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :checkout, only: [:create]
   post "checkout/create", to: "checkout#create"
   root "products#index"
+
+  resources :webhooks, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
